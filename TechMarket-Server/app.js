e9bd0142a43a17f0b0bc9781c59138a81b6ed8cc
Jack Lim <jennystr233@gmail.com>
2021-06-09 23:16:54 +0900

    [fix]/errorhandling & ready to deploy

--- TechMarket-Server/app.js
+++ TechMarket-Server/app.js
@@ -14,11 +14,11 @@ app.use(express.json());
 const port = 8080;
 
 
-app.use(
-  cors({
-    
-  })
-);
+app.use(cors({
+  origin: true,  //요청에 출처가 없음
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true, //JSCODE 노출여부
+}));
 
 
 app.post("/user/login", controllers.logInController);
@@ -26,9 +26,16 @@ app.post("/user/signout", controllers.signOutController);
 
 
 app.get('/', (req, res) => {
-  res.status(201).send('Hello World');
+  res.status(201).send('Hello World 🌈');
 });
 
 app.listen(port, () => {
-  console.log(`서버가 ${port}번에서 작동중입니다.`);
+  console.log(' ');
+  console.log(`|--------------------------------------|`)
+  console.log(`|             TechMarket               |`)
+  console.log(`|                                      |`)
+  console.log(`| 🚀 서버가 ${port}번에서 작동중입니다.🚀 |`);
+  console.log(`|                                      |`)
+  console.log(`|--------------------------------------|`)
+  console.log(' ');
 });
--- TechMarket-Server/controller/index.js
+++ TechMarket-Server/controller/index.js
@@ -7,17 +7,19 @@ const { user } = require("../models"); //미완성 모델
 module.exports = {
   logInController: async (req, res) => {
     //로그인 로직 작성
-    if(!user){
-      res.status(401).send("오류");
-    }
-    if(req.body.email === undefined){
-      console.log(req.body)
-      res.status(401).send("이메일오류");
-    }
+    // if(!user){
+    //   res.status(401).send("오류");
+    // }
+    // if(req.body.email === undefined){
+    //   console.log(req.body)
+    //   res.status(401).send("이메일오류");
+    // }
     
       const userInfo = await user.findOne({
         where: { email : req.body.email, password: req.body.password },
-      });
+      }).catch((err)=>{
+        res.status(500).send(err);
+      })
 
       if (!userInfo) { //로그인 실패
         res.status(401).send("Invalid user or Wrong password");
@@ -51,12 +53,22 @@ module.exports = {
         res.status(200).json( response );
       }
       
+      
+      
       //res.status(201).send("Invalid user or Wrong password");
   },
   signOutController: (req, res) => {
     //로그아웃 로직 작성
-    //로그인 상태검사
-    res.status(200).send("See you next time!");
+    //로그인 상태검사 = boolean 값을 주고 판단을 해야할 것인가?
+    //아니면 express-session(req.session.userid)으로 판단할 것인가 상의하기 *
+    // if(로그인 false일경우 or other way){
+    //  res.status(400).send("You need to login first!" );
+    //}
+    
+    res.status(200).send("See you next time!")
+    .catch((err)=>{
+      res.status(500).send(err);
+    })  
   },
 };
 
