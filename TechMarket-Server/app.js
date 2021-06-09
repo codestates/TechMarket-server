118f785e530cd4cc743aa6c3558efd5d8ab34528
Jack Lim <jennystr233@gmail.com>
2021-06-09 17:20:09 +0900

    init re

--- TechMarket-Server/app.js
+++ TechMarket-Server/app.js
@@ -8,12 +8,15 @@ const app = express();
 app.use(express.json());
 const port = 80;
 
+//테스트용 주석
+
 app.use(
   cors({
     origin: true,
     credentials: true
   })
 );
+
 
 app.get('/', (req, res) => {
   res.status(201).send('Hello World');
@@ -22,4 +25,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`서버가 ${port}번에서 작동중입니다.`);
 });
-
