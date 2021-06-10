5b5f5586ba6775a45513e33ccba412571c4c7557
thislifehyeon <vkhkhv@naver.com>
2021-06-09 21:27:11 -0700

    [server] config

--- TechMarket-Server/config/config.js
+++ TechMarket-Server/config/config.js
@@ -5,21 +5,22 @@ module.exports = {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   }
 }
