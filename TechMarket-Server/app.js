d92e07c3b08ec5056b1fe8f4fe97dc7fc0b4c72d
thislifehyeon <vkhkhv@naver.com>
2021-06-13 04:05:39 -0700

    [댓글 및 기타 수정]

--- TechMarket-Server/app.js
+++ TechMarket-Server/app.js
@@ -19,6 +19,8 @@ require("./models");
 
 const app = express();
 app.use(express.json());
+app.use(express.static('uploadedFiles'));
+
 const port = 8080;
 
 app.set('view engine', 'ejs');
@@ -40,11 +42,14 @@ app.post("/user/modify", controllers.updateUserinfo);     //회원 정보 수정
 //app.post("/mypage/upload", boardcontroller.uploadController); //게시물 작성 아래 uploadImage에 통합
 app.post("/mypage/deletecontent", boardcontroller.deleteController); //게시물 삭제 
 app.post("/mypage/upload", upload.array('photos') , imagecontroller.uploadImage); //게시물 업로드(이미지 함께)
+app.post("/comment/create", boardcontroller.createComment);    //댓글 작성하기
+app.post("/comment/delete", boardcontroller.deleteComment);    //댓글 삭제하기
 
 //
 app.get("/search", searchcontroller.searchController ); //글 검색
 app.get("/products", searchcontroller.showAllboard);    //전체 글 목록 불러오기(사진 제외)
 app.get("/board", searchcontroller.showOneboard);       //게시물 하나 조회
+//app.get("/showimage", searchcontroller.showImage);    //미완성 - 필요 없을 듯
 
 
 app.get('/', (req, res) => {
@@ -60,3 +65,4 @@ app.listen(port, () => {
 //0611 검색기능 완료
 //0611 전체 글 목록 시작
 //0611 전체 글목록 , 글 하나 조회 완료
+//0613 사진 정보 방식 변경 (경로 -> 이름)
